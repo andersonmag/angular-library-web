bba1c4bb9c0a57f06feed8b52fa88ebf25c90915
Anderson <adnrepper@gmail.com>
2020-05-25 14:24:33 -0300

    Edições na página index e inicialização dos componentes no modulo.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BookInfoComponent } from './component/book-info/book-info.component';
+import { BookListComponent } from './component/book-list/book-list.component';
+import { FooterComponent } from './component/template/footer/footer.component';
+import { HeaderComponent } from './component/template/header/header.component';
+import { CardComponent } from './component/template/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    BookInfoComponent,
+    BookListComponent,
+    FooterComponent,
+    HeaderComponent,
+    CardComponent 
+   ],
   imports: [
     BrowserModule
   ],
