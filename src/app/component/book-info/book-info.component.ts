d44abafae3ae812f6aca2f8f153ed9401ad383f4
Anderson <adnrepper@gmail.com>
2020-05-25 14:21:03 -0300

    Componente de informações sobre o livro

---
+++ src/app/component/book-info/book-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-info',
+  templateUrl: './book-info.component.html',
+  styleUrls: ['./book-info.component.css']
+})
+export class BookInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
