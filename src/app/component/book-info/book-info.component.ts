88790fc16eef1e77aa0c668013bf76e8599bceb7
Anderson <adnrepper@gmail.com>
2020-05-26 12:58:33 -0300

    Componente card deletado, cards e informações sobre os livros editadas nos componentes de book-list e book-info

--- src/app/component/book-info/book-info.component.ts
+++ src/app/component/book-info/book-info.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-book-info',
   templateUrl: './book-info.component.html',
   styleUrls: ['./book-info.component.css']
 })
-export class BookInfoComponent implements OnInit {
+export class BookInfoComponent implements OnInit  {
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) document) { }
+
 
   ngOnInit(): void {
+  }
+
+  changeNav(): void {
+    document.getElementById('wrapper').classList.contains('toggled') ? 
+    document.getElementById('wrapper').classList.remove('toggled') :
+    document.getElementById('wrapper').classList.add('toggled')
   }
 
 }
--- src/app/component/book-list/book-list.component.ts
+++ src/app/component/book-list/book-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from 'src/app/model/book';
 
 @Component({
   selector: 'app-book-list',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookListComponent implements OnInit {
 
+  books: Book[]
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.books = [
+      {
+        id: 1, nome: "A Doutrina Secreta - (Vol. V)", autor: "H.P.Blavatsky", 
+        descricao: 
+        "Primeira edição completa, em português, da monumental obra de H.P.Blavatsky, cuja edição original em inglês foilançada simultaneamente em Londres e Nova Iorque no ano de 1888",
+        DataPublicacao:  new Date("31/10/1980"), preco: 29.0, ImagemUrl: "https://images-americanas.b2w.io/produtos/imagens/5326946/5326947_1GG.jpg"
+      },
+      {
+        id: 2, nome: "A Doutrina Secreta - (Vol. V)", autor: "H.P.Blavatsky", 
+        descricao: 
+        "Primeira edição completa, em português, da monumental obra de H.P.Blavatsky, cuja edição original em inglês foilançada simultaneamente em Londres e Nova Iorque no ano de 1888",
+        DataPublicacao:  new Date("31/10/1980"), preco: 29.0, ImagemUrl: "https://images-americanas.b2w.io/produtos/imagens/5326946/5326947_1GG.jpg"
+      },
+      {
+        id: 3, nome: "A Doutrina Secreta - (Vol. V)", autor: "H.P.Blavatsky", 
+        descricao: 
+        "Primeira edição completa, em português, da monumental obra de H.P.Blavatsky, cuja edição original em inglês foilançada simultaneamente em Londres e Nova Iorque no ano de 1888",
+        DataPublicacao:  new Date("31/10/1980"), preco: 29.0, ImagemUrl: "https://images-americanas.b2w.io/produtos/imagens/5326946/5326947_1GG.jpg"
+      },
+      {
+        id: 4, nome: "A Doutrina Secreta - (Vol. V)", autor: "H.P.Blavatsky", 
+        descricao: 
+        "Primeira edição completa, em português, da monumental obra de H.P.Blavatsky, cuja edição original em inglês foilançada simultaneamente em Londres e Nova Iorque no ano de 1888",
+        DataPublicacao:  new Date("31/10/1980"), preco: 29.0, ImagemUrl: "https://images-americanas.b2w.io/produtos/imagens/5326946/5326947_1GG.jpg"
+      }
+    
+    ]
   }
 
 }
