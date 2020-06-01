6c9fb2de50168cb3f85eada8fcc6fc43769ea5e0
Anderson <adnrepper@gmail.com>
2020-06-01 16:06:56 -0300

    Componente com arquivos para página de informações sobre o livro alterado

--- src/app/component/book-info/book-info.component.ts
+++ src/app/component/book-info/book-info.component.ts
@@ -1,23 +1,33 @@
+import { Book } from 'src/app/model/book';
+import { BookService } from './../../service/book.service';
 import { Component, OnInit, Inject } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-book-info',
   templateUrl: './book-info.component.html',
   styleUrls: ['./book-info.component.css']
 })
-export class BookInfoComponent implements OnInit  {
+export class BookInfoComponent implements OnInit {
 
-  constructor(@Inject(DOCUMENT) document) { }
+  book: Book
+  sliceLength: number = 1000
 
+  constructor(private bookService: BookService,
+    private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id = this.activatedRoute.snapshot.paramMap.get("id")
+    this.bookService.obterLivroPorId(id).subscribe(book => {
+      this.book = book
+    })
   }
 
-  changeNav(): void {
-    document.getElementById('wrapper').classList.contains('toggled') ? 
-    document.getElementById('wrapper').classList.remove('toggled') :
-    document.getElementById('wrapper').classList.add('toggled')
+  changeSlice(): void {
+    this.sliceLength == 1000 ?
+      this.sliceLength = this.book.descricao.length :
+      this.sliceLength = 1000
   }
 
 }
