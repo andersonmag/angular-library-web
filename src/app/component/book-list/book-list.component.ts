07b5ae7a53d65b66cb2b53ec3c74cb5dd43dc5d3
Anderson <adnrepper@gmail.com>
2020-06-01 16:11:14 -0300

    Componente de listagem de livros com alterações para colunas e cards

--- src/app/component/book-list/book-list.component.ts
+++ src/app/component/book-list/book-list.component.ts
@@ -1,3 +1,5 @@
+import { ActivatedRoute } from '@angular/router';
+import { BookService } from './../../service/book.service';
 import { Component, OnInit } from '@angular/core';
 import { Book } from 'src/app/model/book';
 
@@ -10,37 +12,13 @@ export class BookListComponent implements OnInit {
 
   books: Book[]
 
-  constructor() { }
+  constructor(private bookService: BookService, private routerActive: ActivatedRoute) { }
 
   ngOnInit(): void {
 
-    this.books = [
-      {
-        id: 1, nome: "A Doutrina Secreta - (Vol. V)", autor: "H.P.Blavatsky", 
-        descricao: 
-        "Primeira edição completa, em português, da monumental obra de H.P.Blavatsky, cuja edição original em inglês foilançada simultaneamente em Londres e Nova Iorque no ano de 1888",
-        DataPublicacao:  new Date("31/10/1980"), preco: 29.0, ImagemUrl: "https://images-americanas.b2w.io/produtos/imagens/5326946/5326947_1GG.jpg"
-      },
-      {
-        id: 2, nome: "A Doutrina Secreta - (Vol. V)", autor: "H.P.Blavatsky", 
-        descricao: 
-        "Primeira edição completa, em português, da monumental obra de H.P.Blavatsky, cuja edição original em inglês foilançada simultaneamente em Londres e Nova Iorque no ano de 1888",
-        DataPublicacao:  new Date("31/10/1980"), preco: 29.0, ImagemUrl: "https://images-americanas.b2w.io/produtos/imagens/5326946/5326947_1GG.jpg"
-      },
-      {
-        id: 3, nome: "A Doutrina Secreta - (Vol. V)", autor: "H.P.Blavatsky", 
-        descricao: 
-        "Primeira edição completa, em português, da monumental obra de H.P.Blavatsky, cuja edição original em inglês foilançada simultaneamente em Londres e Nova Iorque no ano de 1888",
-        DataPublicacao:  new Date("31/10/1980"), preco: 29.0, ImagemUrl: "https://images-americanas.b2w.io/produtos/imagens/5326946/5326947_1GG.jpg"
-      },
-      {
-        id: 4, nome: "A Doutrina Secreta - (Vol. V)", autor: "H.P.Blavatsky", 
-        descricao: 
-        "Primeira edição completa, em português, da monumental obra de H.P.Blavatsky, cuja edição original em inglês foilançada simultaneamente em Londres e Nova Iorque no ano de 1888",
-        DataPublicacao:  new Date("31/10/1980"), preco: 29.0, ImagemUrl: "https://images-americanas.b2w.io/produtos/imagens/5326946/5326947_1GG.jpg"
-      }
-    
-    ]
+      this.bookService.obterTodosOsLivros().subscribe(books => {
+        this.books = books
+      })
   }
 
 }
