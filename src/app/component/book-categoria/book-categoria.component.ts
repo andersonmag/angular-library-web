9909d6501dee739d1dd445da64a17529e69651f3
Anderson <adnrepper@gmail.com>
2020-06-03 01:10:17 -0300

    Conteudo do componente book-categoria alterado junto ao sidebar. Livros por link de categoria sendo buscados e mostrados na pagina

--- src/app/component/book-categoria/book-categoria.component.ts
+++ src/app/component/book-categoria/book-categoria.component.ts
@@ -1,7 +1,9 @@
+import { Categoria } from './../../model/Categoria';
 import { Book } from 'src/app/model/book';
 import { ActivatedRoute, Router } from '@angular/router';
 import { BookService } from './../../service/book.service';
 import { Component, OnInit } from '@angular/core';
+import { parse } from 'path';
 
 @Component({
   selector: 'app-book-categoria',
@@ -11,6 +13,7 @@ import { Component, OnInit } from '@angular/core';
 export class BookCategoriaComponent implements OnInit {
 
   books: Book[]
+  categoria: Categoria
 
   constructor(private bookService: BookService,
     private router: Router, private activatedRoute: ActivatedRoute) { }
@@ -19,10 +22,13 @@ export class BookCategoriaComponent implements OnInit {
 
     this.activatedRoute.params.subscribe(params => {
       const id = params['id'];
-      this.bookService.obterLivroPorCategoria(id).subscribe(books => {
-        this.books = books;
+      this.bookService.obterCategoriaPorLink(id).subscribe(categoria => {
+        this.bookService.obterLivrosPorCategoria(categoria.id + '') .subscribe(books => {
+          this.books = books
+        })
       });
     });
+
   }
 
 }
