361807f4ce478a379c92bdda63165ed8e03b7ffd
Anderson <adnrepper@gmail.com>
2020-06-14 13:46:56 -0300

    Pesquisa funcionando com redirecionamento para BookSearch

--- src/app/component/template/header/header.component.ts
+++ src/app/component/template/header/header.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  q: string = 'soft'
+
+  constructor(private route: Router) { }
 
   ngOnInit(): void {
+  }
+
+  teste(event): void {
+    this.route.navigate(['/book/search/' + event.target.value])
   }
 
 }
