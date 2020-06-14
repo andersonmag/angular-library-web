3973819d172ab6e7a696ff472bfc113a491f3038
Anderson <adnrepper@gmail.com>
2020-06-14 13:43:41 -0300

    Array com links e nomes de icones para o footer

--- src/app/component/template/footer/footer.component.ts
+++ src/app/component/template/footer/footer.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  date: any
+  icones = [{
+    class: '',
+    link: ''
+  }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+    this.date = new Date().getFullYear()
+    this.icones = [{
+      class: "devicon-github-plain colored icone",
+      link: "https://github.com/andersonmag/angular-library-web"
+    },
+    {
+      class: "devicon-heroku-original colored icone ml-1",
+      link: "https://library-angjs.herokuapp.com/"
+    }]
   }
 
 }
