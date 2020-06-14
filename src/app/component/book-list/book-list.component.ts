f7fb30ad1dc86b1df4028b64ed4f1f320b8856ec
Anderson <adnrepper@gmail.com>
2020-06-14 13:41:41 -0300

    Lista de livros com paginação dinâmica, verificação de lista vazia e obtenção por demanda

--- src/app/component/book-list/book-list.component.ts
+++ src/app/component/book-list/book-list.component.ts
@@ -12,18 +12,50 @@ export class BookListComponent implements OnInit {
 
   books: Book[]
   loading: boolean = true
-
-  onLoad() {
-    this.loading = false;
+  q: string = ''
+  result: string
+  config = {
+    itemsPerPage: 0,
+    currentPage: 0,
+    previousLabel: '',
+    nextLabel: '',
+    totalItems: 0
   }
 
   constructor(private bookService: BookService, private routerActive: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.bookService.obterTodosOsLivros().subscribe(books => {
+      this.books = books['content']
+      this.config.totalItems = books['total_size']
+      this.config.itemsPerPage = books['size']
+    })
+  }
 
-      this.bookService.obterTodosOsLivros().subscribe(books => {
-        this.books = books
-      })
+  onLoad() {
+    this.loading = false;
+  }
+
+  onPageChange(event) {
+    this.config.currentPage = event;
+    this.bookService.obterLivrosComPaginacao(this.config.currentPage - 1).subscribe(books => {
+      this.books = books['content']
+    })
+  }
+
+  teste(event): void {
+    this.bookService.pesquisarLivros(event.target.value).subscribe(books => {
+      this.q = ""
+      this.result = ""
+      this.books = books['content']
+      this.config.totalItems = books['total_size']
+      this.config.itemsPerPage = books['size']
+    }, (err) => {
+      this.books = []
+      this.config.totalItems = 0
+      this.result = "Nenhum resultado encontrado para " 
+      this.q = event.target.value
+    })
   }
 
 }
