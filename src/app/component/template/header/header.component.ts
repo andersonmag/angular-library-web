63cb2fa3855849e6b0400ee19b40b125a0942fe4
Anderson <adnrepper@gmail.com>
2020-06-21 01:56:35 -0300

    Metodo para aparecer dropdown e atributos Subject para atualização instantanea de valores

--- src/app/component/template/header/header.component.ts
+++ src/app/component/template/header/header.component.ts
@@ -1,3 +1,5 @@
+import { Item } from './../../../model/item';
+import { Book } from './../../../model/book';
 import { CartService } from './../../../service/cart.service';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
@@ -11,6 +13,8 @@ export class HeaderComponent implements OnInit {
 
   q: string = 'soft'
   cartSize = 0
+  itens: Item[]
+  cartPrecoTotal = 0
 
   constructor(private route: Router, private cartService: CartService) { }
 
@@ -18,6 +22,12 @@ export class HeaderComponent implements OnInit {
 
     this.cartService.totalQtdd.subscribe(qtdd => {
       this.cartSize = qtdd
+      this.itens = this.cartService.itens
+
+      this.cartService.totalPreco.subscribe(total => {
+        this.cartPrecoTotal = total
+      })
+     
     }
     )
   }
@@ -26,16 +36,20 @@ export class HeaderComponent implements OnInit {
     this.route.navigate(['/book/search/' + event.target.value])
   }
 
-  showDrop(): void {
-    var drop = document.getElementById("dropdown")
-      drop.classList.add("showing")
-      drop.classList.add("hiding")
-  }
+  
 
-  removeDrop() {
+  showDrop() {
     var drop = document.getElementById("dropdown")
-    drop.classList.remove("showing")
-    drop.classList.remove("hiding")
+      
+      if(!drop.classList.contains("showing")){
+        drop.classList.add("showing")
+        drop.classList.add("hiding")
+      }
+
+    else {
+      drop.classList.remove("showing")
+      drop.classList.remove("hiding")
+    }
   }
 
 }
