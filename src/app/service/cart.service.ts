a4466170fb271dadf66190d3bd7546513b7aed4c
Anderson <adnrepper@gmail.com>
2020-06-23 17:19:12 -0300

    Novos metodos adicionados para manipulação do array de livros

--- src/app/service/cart.service.ts
+++ src/app/service/cart.service.ts
@@ -1,15 +1,17 @@
 import { Item } from './../model/item';
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Subject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
+
   itens: Item[] = []
   totalQtdd: Subject<number> = new Subject<number>()
   totalPreco: Subject<number> = new Subject<number>()
+  qtdd: number = 0
 
   constructor() { }
 
@@ -25,7 +27,7 @@ export class CartService {
 
     for (let i = 0; i < this.itens.length; i++) {
       if (this.itens[i].id == item.id) {
-        this.itens[i].quantidade += item.quantidade
+        this.itens[i].quantidade += Number(item.quantidade)
         return true
       }
     }
@@ -35,13 +37,53 @@ export class CartService {
 
     let total: number = 0
     let totalQtdd: number = 0
-    
+    this.qtdd = 0
+
     for (let item of this.itens) {
       total += item.quantidade * item.preco
-      totalQtdd += item.quantidade
     }
 
-    this.totalQtdd.next(totalQtdd)
+    for (let i = 0; i < this.itens.length; i++) {
+      this.qtdd += Number(this.itens[i].quantidade);
+    }
+
+    this.totalQtdd.next(this.qtdd)
     this.totalPreco.next(total)
+  }
+
+  deletarTudo() {
+
+    this.itens = []
+    this.calcularPreco()
+  }
+
+  alterarQtdd(item: Item, qtdd) {
+
+    if (qtdd <= 0) {
+      this.remover(item)
+    }
+
+    else if (qtdd < 11) {
+      let index: number = this.itens
+        .findIndex(
+          tempCartItem => tempCartItem.id === item.id
+        );
+
+      this.itens[index].quantidade = qtdd
+      this.calcularPreco()
+
+    }
+  }
+
+  remover(item: Item) {
+    let index: number = this.itens
+      .findIndex(
+        tempCartItem => tempCartItem.id === item.id
+      );
+
+    if (index > -1) {
+      this.itens.splice(index, 1);
+      this.calcularPreco();
+    }
   }
 }
