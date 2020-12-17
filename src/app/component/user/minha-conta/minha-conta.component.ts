f4298ee88de91a981e73b48fa3a8eed9ad2b47ea
Anderson <adnrepper@gmail.com>
2020-12-17 13:00:32 -0300

    Novos componentes de Pedidos e Perfil de Usuario

---
+++ src/app/component/user/minha-conta/minha-conta.component.ts
@@ -0,0 +1,24 @@
+import { UsuarioService } from 'src/app/service/usuario.service';
+import { Usuario } from './../../../model/usuario';
+import { Subject } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-minha-conta',
+  templateUrl: './minha-conta.component.html',
+  styleUrls: ['./minha-conta.component.css']
+})
+export class MinhaContaComponent implements OnInit {
+
+  usuario: Subject<Usuario> = new Subject<Usuario>()
+
+  constructor(private usuarioService: UsuarioService) { }
+
+  ngOnInit(): void {
+    this.usuarioService.getUser().subscribe(
+            usuario => this.usuario.next(usuario as Usuario))
+    
+    console.log(this.usuario)
+  }
+
+}
