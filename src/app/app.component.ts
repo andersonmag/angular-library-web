f4e5e56bb88b250183d8037d5d1df5d281d66ad4
Anderson <adnrepper@gmail.com>
2020-12-17 13:11:03 -0300

    Esconder footer e header quando exibir componentes de Login e Cadastro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  enable:boolean = false
+
+  ngOnInit() {
+    this.enable = location.pathname == '/login' 
+                  || location.pathname == '/cadastro' ? false : true
+  }
 }
