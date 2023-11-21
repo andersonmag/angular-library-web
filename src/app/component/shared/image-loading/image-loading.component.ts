4ae4c08ffa73fd79c23372e2b8459dbdb23789cd
Anderson <andersondel.dev@gmail.com>
2023-11-20 21:48:40 -0300

    Add component load imagem de livros

---
+++ src/app/component/shared/image-loading/image-loading.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+    selector: "image-loading",
+    templateUrl: "./image-loading.component.html"
+})
+
+export class ImageLoadingComponent {
+    imageLoading: boolean = true
+    @Input() imageWidth: Number;
+    @Input() imageUrl: string;
+}
